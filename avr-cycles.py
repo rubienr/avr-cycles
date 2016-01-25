9b071d3b4f6073c5186be377344c3e5544716d86
Raoul Rubien <rubienr@gmail.com>
2016-01-25 08:56:45 +0100

    fixed missing absolute config file path

--- avr-cycles.py
+++ avr-cycles.py
@@ -10,12 +10,12 @@ import sys
 import csv
 import json
 
-config = json.load(open("avr-cycles.conf"))
-
-tableFolder = "/" + config["instructionTablesFolder"] + "/"
+scriptPath = sys.path[0]
+config = json.load(open(scriptPath + "/avr-cycles.conf"))
+tableFolder = sys.path[0] + "/" + config["instructionTablesFolder"] + "/"
 table = config["instructionTable"]
 
-reader = csv.reader(open(sys.path[0] + tableFolder + table, "r"))
+reader = csv.reader(open(tableFolder + table, "r"))
 dictionary = {}
 for k,v in reader:
     if k in dictionary:
