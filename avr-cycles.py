61082f613893f37104c2341353020774435460a7
Raoul Rubien <rubienr@gmail.com>
2016-07-26 21:57:02 +0200

    implemented optional -mmcu=<mmcu> cli argument

--- avr-cycles.py
+++ avr-cycles.py
@@ -2,8 +2,9 @@
 
 '''
     usage: avr-objcump -zS firmware.elf | python avr-cycles.py
+    usage: avr-objcump -zS firmware.elf | python avr-cycles.py --mmcu=<mmcu>
     @author: raoul rubien 
-    01/2016
+    07/2016
 '''
 
 import sys
@@ -15,6 +16,16 @@ config = json.load(open(scriptPath + "/avr-cycles.conf"))
 tableFolder = sys.path[0] + "/" + config["instructionTablesFolder"] + "/"
 table = config["instructionTable"]
 
+# overwrite default value if specified per cli: --mmcu=newMcu
+if len(sys.argv) == 2:
+    mmcu = sys.argv[1]
+    print("1 mmcu: %s" % mmcu)
+    if "-mmcu=" in mmcu:
+        mmcu = mmcu.replace("-mmcu=", "")
+    print("mmcu: %s" % mmcu)
+    table = config[mmcu]
+
+# read lookup table
 reader = csv.reader(open(tableFolder + table, "r"))
 dictionary = {}
 for k,v in reader:
@@ -23,7 +34,7 @@ for k,v in reader:
     else:
         dictionary[k] = v
 	
-
+# translate stdin
 for line in sys.stdin:
     for k in dictionary.keys():
         line = line.replace("\t"+str.lower(k)+"\t", "\t[[%s -> %s]]\t" %(k, dictionary[k]))
